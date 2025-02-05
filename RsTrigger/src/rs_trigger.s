45f6676b7f9f481399e610ba3638ba8b7b322e0b
Dmitry Antontsev <antontsev.dmitry@yandex.ru>
2025-02-05 22:22:03 +0300

    Made rs trigger with basic IO

--- RsTrigger/src/rs_trigger.s
+++ RsTrigger/src/rs_trigger.s
@@ -15,21 +15,52 @@ out DDRB, r16
 ldi r17, 0x8
 out PORTB, r17
 
+ldi r19, 0x8    ; State register
+
 ; Poling pin 3 waiting for LOW on it
-loop:
-    in r20, PINB    ; take value from PINB
-    andi r20, 0x8   ; mask r18 with 0x8 
-    cpi r20, 0x0    ; compare r18 with 0
-    breq turn_on    ; if 0 on PB3 turn PB4 on
+LOW_waiting_loop:
+    in r20, PINB           ; take value from PINB
+    andi r20, 0x8          ; mask r20 with 0x8 
+    cpi r20, 0x0           ; compare r20 with 0
+    breq HIGH_waiting_loop ; if 0 on PB3 turn PB4 on
 
     ; Set PB3 in HIGH and other in LOW
     ldi r17, 0x8
     out PORTB, r17
 
-    rjmp loop
-turn_on:
+    rjmp LOW_waiting_loop
+
+HIGH_waiting_loop:
     ; Set PB3, PB4 in HIGH and other in LOW
     ldi r17, 0x18
     out PORTB, r17
 
-    rjmp loop
+    in r20, PINB           ; take value from PINB
+    andi r20, 0x8          ; mask r20 with 0x8 
+    cpi r20, 0x0           ; compare r20 with 0
+    brne next_LOW_waiting_loop ; if 1 on PB3 goto next LOW waiting loop
+    rjmp HIGH_waiting_loop
+
+next_LOW_waiting_loop:
+    in r20, PINB           ; take value from PINB
+    andi r20, 0x8          ; mask r20 with 0x8 
+    cpi r20, 0x0           ; compare r20 with 0
+    breq next_HIGH_waiting_loop ; if 1 on PB3 goto next HIGH waiting loop
+
+    ; Set PB3, PB4 in HIGH and other in LOW
+    ldi r17, 0x18
+    out PORTB, r17
+
+    rjmp next_LOW_waiting_loop
+
+next_HIGH_waiting_loop:
+    ; Set PB3, PB4 in HIGH and other in LOW
+    ldi r17, 0x8
+    out PORTB, r17
+
+    in r20, PINB           ; take value from PINB
+    andi r20, 0x8          ; mask r20 with 0x8 
+    cpi r20, 0x0           ; compare r20 with 0
+    brne LOW_waiting_loop ; if 1 on PB3 goto next LOW waiting loop
+
+    rjmp next_HIGH_waiting_loop
